d66348ff5c933a3934bdb4ab13166cc2fa1c241f
Jaeyoung Ju <101681367+JAEYOUNGJU@users.noreply.github.com>
2022-07-15 08:02:57 +0900

    AOP and Transaction

---
+++ src/main/java/kr/ac/kopo/ctc/spring/board/service/BoardItemServiceImpl.java
@@ -0,0 +1,69 @@
+package kr.ac.kopo.ctc.spring.board.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.ac.kopo.ctc.spring.board.domain.BoardItem;
+import kr.ac.kopo.ctc.spring.board.repository.BoardItemRepository;
+
+@Service
+public class BoardItemServiceImpl implements BoardItemService {
+
+	@Override
+	public void test() {
+		System.out.println("BoardItemServiceImpl.test() 메소드 호출");
+	}
+
+	@Override
+	public void testAopBefore() {
+		System.out.println("BoardItemServiceImpl.testAopBefore() 메소드 호출");
+	}
+
+	@Override
+	public void testAopAfter() {
+		System.out.println("BoardItemServiceImpl.testAopAfter() 메소드 호출");
+	}
+
+	@Override
+	public String testAopAfterReturning() {
+		System.out.println("BoardItemServiceImpl.testAopAfterReturning() 메소드 호출");
+		return "Success";
+	}
+
+	@Override
+	public void testAopAfterThrowing() {
+		System.out.println("BoardItemServiceImpl.testAopAfterThrowing() 메소드 호출");
+		throw new RuntimeException("runtime exception 발생");
+	}
+
+	@Override
+	public void testAopAround() {
+		System.out.println("BoardItemServiceImpl.testAopAround() 메소드 호출");
+	}
+
+	@Autowired
+	BoardItemRepository boardItemRepository;
+
+	@Override
+	public String testNoTransactional() {
+		BoardItem boardItem = boardItemRepository.findById(3L).get();
+		boardItem.setTitle("update - no transactional");
+		boardItemRepository.save(boardItem);
+
+		throw new RuntimeException("Spring Boot No Transactional Test");
+
+	}
+
+	@Override
+	@Transactional
+	public String testTransactional() {
+		BoardItem boardItem = boardItemRepository.findById(4L).get();
+		boardItem.setTitle("update - transactional");
+		boardItemRepository.save(boardItem);
+
+		throw new RuntimeException("Spring Boot Transactional Test");
+	}
+
+}
