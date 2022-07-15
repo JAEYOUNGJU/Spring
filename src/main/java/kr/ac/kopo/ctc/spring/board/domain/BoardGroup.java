e0b46a35a22d1761f96b32689ac027f27bd41c8f
Jaeyoung Ju <101681367+JAEYOUNGJU@users.noreply.github.com>
2022-07-15 17:43:12 +0900

    주석 수정

--- src/main/java/kr/ac/kopo/ctc/spring/board/domain/BoardGroup.java
+++ src/main/java/kr/ac/kopo/ctc/spring/board/domain/BoardGroup.java
@@ -19,7 +19,8 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 public class BoardGroup {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 생성할 때 @GeneratedValue 어노테이션이 함께 사용됨
+	// 기본키를 자동으로 생성할 때 @GeneratedValue 어노테이션이 함께 사용됨
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
 	private Long id;
 	@Column
@@ -40,12 +41,14 @@ public class BoardGroup {
 	public void setName(String name) {
 		this.name = name;
 	}
-	@JsonManagedReference
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "boardGroup") // mappedBy 양방향 관계 설정시 관계의 주체가 되는 쪽에서 정의함
-	// 부모입장에서 fetch = FetchType.LAZY 은 기본값
-	//CascadeType.ALL 이 설정이 되어있으면 boardgroup 이 지울 때 그 안에 있는 boardItem들도 지워지게 되는 것
 
-	private List<BoardItem> boardItems; //BoardItem 호출
+	@JsonManagedReference
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "boardGroup")
+	// mappedBy 양방향 관계 설정시 관계의 주체가 되는 쪽에서 정의함
+	// 부모입장에서 fetch = FetchType.LAZY 은 기본값
+	// CascadeType.ALL 이 설정이 되어있으면 boardgroup 이 지울 때 그 안에 있는 boardItem들도 지워지게 되는 것
+
+	private List<BoardItem> boardItems; // BoardItem 호출
 
 	public List<BoardItem> getBoardItems() {
 		if (boardItems == null) {
@@ -72,4 +75,3 @@ public class BoardGroup {
 		return result;
 	}
 }
-
