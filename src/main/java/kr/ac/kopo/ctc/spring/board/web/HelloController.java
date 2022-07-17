989134a42876c89c6c54553c81c8de6fbe329894
Jaeyoung Ju <101681367+JAEYOUNGJU@users.noreply.github.com>
2022-07-17 17:26:25 +0900

    주석 추가

--- src/main/java/kr/ac/kopo/ctc/spring/board/web/HelloController.java
+++ src/main/java/kr/ac/kopo/ctc/spring/board/web/HelloController.java
@@ -16,19 +16,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import kr.ac.kopo.ctc.spring.board.domain.BoardItem;
 
 @Controller
-@RequestMapping(value = "/boardItem")
+@RequestMapping(value = "/boardItem") //공통적인 URL은 class에 @RequestMapping으로 설정
 public class HelloController {
 		
-	@RequestMapping(value = "") //boardItem 이후에 나올 아래의 url 지정
+	//boardItem 이후에 나올 URL 지정
+	@RequestMapping(value = "") // /boardItem 만 입력할 경우 뜨는 출력할 화면 설정
 	public String helloSpringBoot (Model model) {
 		model.addAttribute("name", "주재영");
 		return "hello";
 	}
 	
-	@RequestMapping(value = "/getParameter")
+	@RequestMapping(value = "/getParameter") // URL부분에 /boardItem/get.Parameter 입력할 경우 
 	public String getParameter(Model model,HttpServletRequest req) {
-		String author = req.getParameter("name");
-		model.addAttribute("name", author);
+		String author = req.getParameter("name");// 객체 req에 저장된 name 데이터를 꺼냄
+		model.addAttribute("name", author); //해당 메서드를 이용해 view에 전달할 데이터를 name, author 으로 묶어 전달 
 		return "hello";
 	}
 	@RequestMapping(value = "/requestParam")
@@ -46,6 +47,8 @@ public class HelloController {
 		model.addAttribute("name", boardItem.getAuthor());
 		return "hello";
 	}
+	
+	//스프링에서 비동기 처리를 하는 경우 RequestBody
 	@RequestMapping(value = "/requestBody1")
 	public String requestBody1(Model model, @RequestBody Map<String, Object> obj) {
 		model.addAttribute("name", obj.get("name"));
